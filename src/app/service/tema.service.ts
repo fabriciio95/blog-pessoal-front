044a738c402897ba3f4f0e1fb5a49f75aa087434
fabriciio95 <fabriciusiqueira@gmail.com>
2021-07-02 11:30:06 -0300

    Criando crud de tema

---
+++ src/app/service/tema.service.ts
@@ -0,0 +1,37 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment.prod';
+import { Tema } from '../model/Tema';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TemaService {
+
+  constructor(private http: HttpClient) { }
+
+  token = {
+    headers: new HttpHeaders().set('Authorization', environment.token)
+  }
+
+  getAllTema(): Observable<Tema[]> {
+    return this.http.get<Tema[]>(`${environment.host}/temas`, this.token)
+  }  
+
+  getById(id: number): Observable<Tema> {
+    return this.http.get<Tema>(`${environment.host}/temas/${id}`, this.token)
+  }
+
+  postTema(tema: Tema): Observable<Tema> {
+    return this.http.post<Tema>(`${environment.host}/temas`, tema, this.token)
+  }
+
+  putTema(tema: Tema): Observable<Tema> {
+    return this.http.put<Tema>(`${environment.host}/temas`, tema, this.token)
+  }
+
+  deleteTema(id: number) {
+    return this.http.delete(`${environment.host}/temas/${id}`, this.token)
+  }
+}
